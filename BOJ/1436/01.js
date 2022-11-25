3f4b9ac5300212af043db88c391dccc399a5a09e
JISUN HAN <my__memoria@naver.com>
2022-11-25 20:52:22 +0900

    fix: solution 함수로 변경

--- BOJ/1436/01.js
+++ BOJ/1436/01.js
@@ -1,12 +1,20 @@
 // const N = +require("fs").readFileSync("/dev/stdin").toString();
-const N = 500;
 
-let num = 666;
-let count = 1;
+function solution(N) {
+  let num = 666;
+  let count = 1;
 
-while (count !== N) {
-  num++;
-  if (String(num).includes("666")) count++;
+  while (count !== N) {
+    num++;
+    if (String(num).includes("666")) count++;
+  }
+
+  return num;
 }
 
-console.log(num);
+// console.log(solution(N));
+console.log(solution(2));
+console.log(solution(3));
+console.log(solution(6));
+console.log(solution(187));
+console.log(solution(500));

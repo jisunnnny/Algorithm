92290a8c3905f95004c21e7974cd936d4ca88bee
JISUN HAN <88502596+jisunnnny@users.noreply.github.com>
2022-11-25 19:57:11 +0900

    [Silver V] Title: 영화감독 숌, Time: 452 ms, Memory: 23248 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -0,0 +1,10 @@
+const N = +require("fs").readFileSync("/dev/stdin").toString()
+let num = 666;
+let count = 1;
+
+while (count !== N) {
+    num++;
+    if (String(num).includes("666")) count++;
+}
+
+console.log(num);

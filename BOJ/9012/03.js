042f45430c40d4965098d5ce4d2a1c94b15322c5
JISUN HAN <my__memoria@naver.com>
2022-11-29 15:40:28 +0900

    feat: BOJ 9012번 문제 replace와 includes를 사용한 풀이

---
+++ BOJ/9012/03.js
@@ -0,0 +1,28 @@
+const input = ["6", "(())())", "(((()())()", "(()())((()))", "((()()(()))(((())))()", "()()()()(()()())()", "(()((())()("];
+// const input = ["3", "((", "))", "())(()"];
+
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+// replace이용해서 풀기
+function solution(input) {
+  const length = input[0];
+  const resultArr = [];
+
+  for (let i = 0; i < length; i++) {
+    let brackets = input[i + 1];
+
+    while (brackets.includes("()")) {
+      brackets = brackets.replace("()", "");
+    }
+
+    if (brackets.length === 0) {
+      resultArr.push("YES");
+    } else {
+      resultArr.push("NO");
+    }
+  }
+
+  return resultArr;
+}
+
+console.log(solution(input));

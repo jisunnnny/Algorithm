9f73d3d11c0ae616375b5ee0b8e7d93d63212742
JISUN HAN <my__memoria@naver.com>
2022-11-29 15:40:52 +0900

    feat: BOJ 9012번 정규식 이용한 풀이

---
+++ BOJ/9012/04.js
@@ -0,0 +1,28 @@
+const input = ["6", "(())())", "(((()())()", "(()())((()))", "((()()(()))(((())))()", "()()()()(()()())()", "(()((())()("];
+// const input = ["3", "((", "))", "())(()"];
+
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+// 정규식 이용한 풀이
+function solution(input) {
+  const length = input[0];
+  const resultArr = [];
+
+  for (let i = 0; i < length; i++) {
+    let brackets = input[i + 1];
+
+    while (brackets.includes("()")) {
+      brackets = brackets.replace(/\(\)/g, "");
+    }
+
+    if (brackets.length === 0) {
+      resultArr.push("YES");
+    } else {
+      resultArr.push("NO");
+    }
+  }
+
+  return resultArr;
+}
+
+console.log(solution(input));

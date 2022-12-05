642fb1f255996efb2483c8f8cc00a46d77b89e73
JISUN HAN <my__memoria@naver.com>
2022-12-05 19:32:59 +0900

    BOJ 1002번 문제 풀이 수정

--- BOJ/1992/01.js
+++ BOJ/1992/01.js
@@ -1,17 +1,27 @@
-// const input = +require("fs").readFileSync("/dev/stdin").toString();
-const input = ["8", "11110000", "11110000", "00011100", "00011100", "11110000", "11110000", "11110011", "11110011"];
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const input = ["8", "00000000", "00000000", "00001111", "00001111", "00011111", "00111111", "00111111", "00111111"];
+// const input = ["8", "11110000", "11110000", "00011100", "00011100", "11110000", "11110000", "11110011", "11110011"];
+
 const N = Number(input[0]);
 const nums = input.slice(1).map((v) => v.split("").map(Number));
+
 const results = [];
 
 function solution(N, x, y) {
   let count = 0;
 
+  console.log(N, x, y);
+
   for (let i = 0; i < N; i++) {
+    console.log("startLine", i);
     for (let j = 0; j < N; j++) {
-      count += nums[x + i][y + j];
+      console.log(nums[y + j][x + i]);
+      count += nums[y + j][x + i];
     }
   }
+
+  console.log("count", count);
 
   if (count === 0) {
     results.push(0);
